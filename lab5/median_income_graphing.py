ff11547f98441be1d86320a91474f6aee4231d95
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2022-02-06 15:51:58 -0800

    First mean sampling test

--- lab5/median_income_graphing.py
+++ lab5/median_income_graphing.py
@@ -1,5 +1,21 @@
 import graphing
 import pandas as pd
 
-if __name__ == "__main__":
-    
+incomes = pd.read_csv('medIncome.csv')
+backup = incomes.copy()
+
+mean = 0
+sampleMeans = []
+
+if __name__ == '__main__':
+    for a in range(0, 50):
+        incomes = backup.copy()
+        incomes.shuffle()
+
+        mean = 0
+        for b in range(0, 100):
+            mean += incomes.pop()
+        mean = mean / 100
+        sampleMeans.append(mean)
+
+        print('Mean ' + str(a) + ' to: ' + str(mean))
