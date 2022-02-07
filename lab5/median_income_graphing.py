e344cc754a92b3153c8381571a70f4d25b817361
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2022-02-06 16:50:16 -0800

    LA income normals working

--- lab5/median_income_graphing.py
+++ lab5/median_income_graphing.py
@@ -1,39 +1,38 @@
 import graphing
 import random
 
+import os
+
 import numpy as np
 import pandas as pd
 
 incomes = pd.read_csv('lab5/medIncome.csv')
 
-mean = 0
-sampleMeans = []
-
-def central_limit_histogram(incomes, samples, trials):
+def central_limit_histogram(incomes, samples, trials, filen, directory):
     assert type(samples) == int, (str(samples) + ' must be a positive integer!')
     assert type(trials) == int, (str(trials) + ' must be a positive integer!')
 
     assert samples > 0, (str(samples) + ' must be a positive integer!')
     assert trials > 0, (str(samples) + ' must be a positive integer!')
 
-    for a in range(0, samples):
+    mean = 0
+    sampleMeans = []
 
+    for a in range(0, samples):
         for b in range(0, trials):
+
             income = incomes.sample().mean(0).to_dict()
             mean += income['x']
+
         mean = mean / 100
         sampleMeans.append(mean)
 
         print('Mean ' + str(a) + ' to: ' + str(mean))
+
+    if not os.path.exists('Graphs/'+directory):
+        os.makedirs('Graphs/'+directory)
+
+    graphing.makeHist(sampleMeans, np.arange(0,100000,2000), (str(directory)+'/'+filen))
 
 if __name__ == '__main__':
-    for a in range(0, 50):
-
-        for b in range(0, 100):
-            income = incomes.sample().mean(0).to_dict()
-            mean += income['x']
-        mean = mean / 100
-        sampleMeans.append(mean)
-
-        print('Mean ' + str(a) + ' to: ' + str(mean))
-    graphing.makeHist(sampleMeans, np.arange(0,100000,2000), "test.png")
+    central_limit_histogram(incomes, 50, 100, "normal1.png", "la_incomes_normals")
