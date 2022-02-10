4387fb93843ce1d46db71c359a5312ec8aa86bf4
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2022-02-09 22:23:58 -0800

    Added comments to graphingpy

--- lab5/graphing.py
+++ lab5/graphing.py
@@ -53,9 +53,10 @@ def makeHist(mySamples, binSize = np.arange(-5,5,0.1), fileName='histogram.png')
 
     Returns 
     --
-    None, but saves a histogram
+    None, but saves a histogram in Graphs folder
     '''
 
+    # Creates histogram out of samples, saves it in Graphs folder under fileName.
     plt.hist(mySamples, bins=binSize, density=True)
     plt.savefig("Graphs/" + fileName)
     plt.close()
@@ -72,9 +73,10 @@ def makeBoxplot(mySamplesL, fileName='boxplot.png'):
 
     Returns 
     --
-    None, but saves a boxplot
+    None, but saves a boxplot in Graphs folder
     '''
 
+    # Creates boxplot out of samples, saves it in Graphs folder under fileName.
     plt.boxplot(mySamplesL, notch=True)
     plt.savefig("Graphs/" + fileName)
     plt.close()
@@ -96,25 +98,44 @@ def repeatRandomSamples(trials=10, mean=0, sd=1, sampleSize=1000, binSize = np.a
         The sample size of each individual trial
     binSize: range 
         Defaults to bins of width 0.1 ranging from -5 to +5
+    foldername: str
+        The name of the folder in which to save our random sampling
 
     Returns 
     --
     None, but creates histograms and a boxplot
     '''
+
+    ''' Creates folder by "folderName" in Graphs folder to hold 
+        sample results graphs. '''
+
     if not os.path.exists('Graphs/'+folderName):
         os.makedirs('Graphs/'+folderName)
 
+    # Randomized samples list defined here
     samples = []
+
+    # Run 'trials' number of normal curve samplings
     for i in range(0,trials):
+
+        ''' Generates normal curve probability centering around mean with
+            standard deviation 'sd' and 'sampleSize' sample size. '''
+        
         temp = np.random.normal(mean, sd, sampleSize)
+
+        ''' Append normal list to sample list, and make histogram of 
+        normal curve list. '''
 
         samples.append(temp)
         makeHist(temp, binSize, (folderName + "/normal" + str(i) + ".png"))
 
+    # Make boxplot from list of normal curve samples
     makeBoxplot(samples, (folderName + "/cumulative_normals.png"))
 
-
+# MAIN CODE BLOCK
 if __name__ == '__main__':
+
+    # Make random samplings as directed in lab 5 directions.
     repeatRandomSamples(folderName="original")
 
     repeatRandomSamples(mean=0, sd=1, sampleSize=15, folderName="2-1")
